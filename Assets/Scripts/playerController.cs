2e6b7385e1f3dd10a5fb986f436dd52a701d79a4
seregero00 <seregero18@gmail.com>
2024-10-05 22:28:49 +0200

    Movement, Sprites, Score

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    Rigidbody2D bird;
+
+    int score = 0;
+
+    bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bird = transform.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && !dead){
+            bird.velocity = new Vector2(0, 8.5f);
+        }
+        if (Input.GetKeyDown("r")){
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        dead = true;
+        score = 0;
+        scoreText.text = "0";
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "pointTrigger")
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+}
