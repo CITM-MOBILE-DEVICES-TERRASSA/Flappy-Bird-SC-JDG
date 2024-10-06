e69cf3d814b8cf0fbe8d204ff917ae23522cce61
seregero00 <seregero18@gmail.com>
2024-10-06 23:33:48 +0200

    Done!

--- Assets/Scripts/pipeGenerator.cs
+++ Assets/Scripts/pipeGenerator.cs
@@ -21,7 +21,7 @@ public class pipeGenerator : MonoBehaviour
         {
             timer = 0;
             GameObject pipe = Instantiate (pipes,new Vector2(pipes.transform.position.x, pipes.transform.position.y + Random.Range(-2f,1.8f)),pipes.transform.rotation);
-            Destroy(pipe,5f);
+            Destroy(pipe,12f);
         }
     }
 }
